5d01919d4cd6db3b6af462b761b51ac1848b23ed
CXK-MM <54938994+CXK-MM@users.noreply.github.com>
2021-09-28 15:18:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
+import argparse
 
-fd = open('111.txt', "w", encoding='utf-8') # 文件创建111文件
-fd.write('dfsdfgsdgsd') # 文件保存
-print('1345649')
+
+parser = argparse.ArgumentParser(description="Demo of argparse")
+parser.add_argument('-token','--token', default=' 5 ')
+# parser.add_argument('-y','--year', default='20')
+args = parser.parse_args()
+# print(args)
+a = args.token
+# b = args.year
+
+# print(type(a))
+# print(a+b)
+print(a)
