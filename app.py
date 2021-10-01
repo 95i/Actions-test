868a6fe7aa818f6e83570e7ad11c9c0f0ee881dd
CXK-MM <54938994+CXK-MM@users.noreply.github.com>
2021-10-01 10:27:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,51 @@
-import argparse
-import telegram
+import requests
+import parsel
+import re
+
+# 链接
+# a = input("请输入链接：")
+url = requests.get('https://madou.club/md0178.html').text
+# url = requests.get(a).text
+# print(url)
 
 
-parser = argparse.ArgumentParser(description="Demo of argparse")
-parser.add_argument('-token','--token', default=' 5 ')
-# parser.add_argument('-y','--year', default='20')
-args = parser.parse_args()
-# print(args)
-a = args.token
-# b = args.year
+# xpath提取视频title
+paers = parsel.Selector(url)
+title = str(paers.xpath('//title/text()')).replace("[<Selector xpath='//title/text()' data='", '').replace("'>]", '').replace('/', '') # xpath选择
+print(title)
 
-# print(type(a))
-# print(a+b)
-print(a)
-bot = telegram.Bot(token=a)
-bot.send_message(chat_id='-405240242', text="00000")
-bot.send_video(chat_id='-405240242', video=open('1.mp4', 'rb'), timeout=10000)
+
+# xpath提取视频url
+paers = parsel.Selector(url)
+video_url = str(paers.xpath('.//article[@class="article-content"]//p/iframe/@src').extract()).replace("['", "").replace("']", "") # xpath选择
+print(video_url)
+
+# 获取token
+token = requests.get(video_url).text
+# print(token)
+
+# 提取token
+token_01 = str(re.findall(u'token(.+?)";'.encode('utf8'),token.encode('utf8'))).replace("[b' = ", '').replace("']", '').replace('"', '')# 正则表达式获取关键字字段
+# print(c)
+
+# 访问m3u8文件
+m3u8 = video_url.replace('share', 'videos') + '/index.m3u8?token=' + token_01
+m3u8_url = requests.get(m3u8).text
+# print(m3u8_url)
+
+# 提取key
+key = video_url.replace('share', 'videos')
+print(key)
+
+
+# 保存m3u8文件
+fd = open(title + '.m3u8', "w", encoding='utf-8') # 文件创建111文件
+key11 = video_url.replace('share', 'videos').replace('dash.madou.club', 'dash.madou.club/')
+fd.write(m3u8_url.replace(key11, '').replace('/ts.key', 'ts.key'))
+
+# 下载ts
+fd = open('down.sh', "w", encoding='utf-8')
+fd.write(m3u8_url.replace('index','wget ' + key + '/index'))
+
+
 
