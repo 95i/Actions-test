a86da14485672aaad7de62cc13df31b94a19fdff
CXK-MM <54938994+CXK-MM@users.noreply.github.com>
2021-10-17 20:59:31 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,61 @@
+import requests
+import parsel
+import re
+import os
+
+# 链接
+# a = input("请输入链接：")
+url = requests.get('https://madou.club/md0178.html').text
+# url = requests.get(a).text
+# print(url)
+
+
+# xpath提取视频title
+paers = parsel.Selector(url)
+title = str(paers.xpath('//title/text()')).replace("[<Selector xpath='//title/text()' data='", '').replace("'>]", '').replace('/', '') # xpath选择
+print(title)
+
+
+# xpath提取视频url
+paers = parsel.Selector(url)
+video_url = str(paers.xpath('.//article[@class="article-content"]//p/iframe/@src').extract()).replace("['", "").replace("']", "") # xpath选择
+print(video_url)
+
+aaa = video_url.replace('https://dash.madou.club/share/', '')
+print(aaa)
+
+# 获取token
+token = requests.get(video_url).text
+# print(token)
+
+# 提取token
+token_01 = str(re.findall(u'token(.+?)";'.encode('utf8'),token.encode('utf8'))).replace("[b' = ", '').replace("']", '').replace('"', '')# 正则表达式获取关键字字段
+# print(c)
+
+# 访问m3u8文件
+m3u8 = video_url.replace('share', 'videos') + '/index.m3u8?token=' + token_01
+m3u8_url = requests.get(m3u8).text
+# print(m3u8_url)
+
+# 提取key
+key = video_url.replace('share', 'videos')
+print(key)
+
+# 保存key
+key2 = requests.get(key + '/ts.key').text
+fd = open('ts.key', "w", encoding='utf-8')
+fd.write(key2)
+
+# 保存m3u8文件
+fd = open(title + '.m3u8', "w", encoding='utf-8') # 文件创建111文件
+key11 = video_url.replace('share', 'videos').replace('dash.madou.club', 'dash.madou.club/')
+fd.write(m3u8_url.replace(key11, '').replace('/ts.key', 'ts.key'))
+
+# 下载ts
+fd = open('down.sh', "w", encoding='utf-8')
+fd.write(m3u8_url.replace('index','wget -P ./'+ aaa + ' ' + key + '/index'))
+
+
+print(os.system("chmod 777 down.sh"))
+print(os.system("./down.sh"))
+
